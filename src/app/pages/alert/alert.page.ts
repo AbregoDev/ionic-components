95a486145f523266624eec176eeb9d6edd5f625e
Bryan Moreno <abre.go@outlook.com>
2023-10-16 11:20:05 -0600

    feat: Add alert

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,15 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-alert',
-  templateUrl: './alert.page.html',
-  styleUrls: ['./alert.page.scss'],
+    selector: 'app-alert',
+    templateUrl: './alert.page.html',
+    styleUrls: ['./alert.page.scss'],
 })
-export class AlertPage implements OnInit {
+export class AlertPage {
 
-  constructor() { }
+    constructor(private alertController: AlertController) { }
 
-  ngOnInit() {
-  }
+    async presentAlert() {
+        const alert = await this.alertController.create({
+            header: 'Alert',
+            subHeader: 'Important message',
+            message: 'This is an alert!',
+            buttons: ['Ok'],
+            backdropDismiss: false,
+        });
 
+        await alert.present();
+    }
+
+    async presentAlertMulti() {
+        const alert = await this.alertController.create({
+            header: 'Alert',
+            message: 'Multi buttons with handlers!',
+            buttons: [
+                {
+                    text: 'Ok',
+                    handler: () => { console.log('\'Ok\' button pressed!') }
+                },
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    handler: () => { console.log('\'Cancel\' button pressed!') }
+                },
+            ],
+            backdropDismiss: false,
+        });
+
+        await alert.present();
+    }
+
+    async presentAlertForm() {
+        const alert = await this.alertController.create({
+            header: 'Alert',
+            message: 'Fill the form',
+            inputs: [
+                {
+                    label: 'nombre',
+                    name: 'nombre_field',
+                    placeholder: 'Name',
+                },
+                {
+                    placeholder: 'Nickname (max 8 characters)',
+                    attributes: {
+                        maxlength: 8,
+                    },
+                },
+                {
+                    type: 'number',
+                    placeholder: 'Age',
+                    min: 1,
+                    max: 100,
+                },
+                {
+                    type: 'textarea',
+                    placeholder: 'A little about yourself',
+                },
+            ],
+            buttons: [
+                {
+                    text: 'Ok',
+                    handler: console.log,
+                },
+                'Cancel'
+            ],
+            backdropDismiss: false,
+        });
+
+        await alert.present();
+    }
 }
