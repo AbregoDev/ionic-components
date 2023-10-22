388dd143a8135e1f84cde7c48900116eb88e95fb
Bryan Moreno <abre.go@outlook.com>
2023-10-21 20:17:26 -0600

    feat: Add app components menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from './interfaces/menu-item.interface';
+import { DataService } from './services/data.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: 'app.component.html',
 })
-export class AppComponent {
-    constructor() { }
+export class AppComponent implements OnInit {
+
+    menuItems: MenuItem[] = [];
+
+    constructor(private dataService: DataService) { }
+
+    ngOnInit(): void {
+        this.menuItems = this.dataService.getMenuItems();
+    }
 }
